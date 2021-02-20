8f341c48a67727c5954b15caecde204c900a1c47
Volkan <volkanisik@live.dk>
2021-02-20 03:09:49 +0100

    dpkg installation er næsten færdig

--- install.sh
+++ install.sh
@@ -1,28 +1,53 @@
 #!/bin/bash
 
+echo "Welcome to package installer wizard!"
+
 read -p "Which package do you wish to install? " VAR1
 
 dpkg -s $VAR1 &> /dev/null
     
-if ! [ $? -ne 0 ]
+if ! [ $? -ne 0 ]  # kontrol om exit status for den sidste kørte kommando
     then #Not installed
         echo "$VAR1 is already installed! Aborting!"
         exit        
 fi
 
-read -p "Install from source or with dpkg/rpm? " INSTALL_TYPE
+read -p "Install from source or with dpkg/rpm?  d:Dpkg or s:Source " INSTALL_TYPE
 
 read -p "Link to file download: " FILE_LINK
 
-chmod -R 777 /usr/local/src
 
-#wget "$FILE_LINK" -P /usr/local/src
+PERMISSON=$(stat -c "%a" /usr/local/src)
 
-if [[ "$INSTALL_TYPE" == "dpkg" ]]; then
+if [ "$PERMISSON" -ne 777 ]; then
+
+    chmod -R 777 /usr/local/src
+
+fi
+
+cd /usr/local/src
+
+wget "$FILE_LINK"
+FILE=$(ls -c | head -n1)
+
+if [[ "$INSTALL_TYPE" == "d" ]]; then
     echo "dpkg selected"
+    dpkg -i "$FILE" 2> error.log
 
-    
-    #dpkg -i /usr/local/src/$VAR1.deb
+    if [ $? -ne 0 ]; then
+        cat error.log | egrep -o "'[a-z0-9-]+'" > dependencies
+         for line in $(cat dependencies); do
+          echo "Dependencies missing please install: $line"
+
+          read FILE_LINK_DEPENDENCY
+
+          wget "$FILE_LINK_DEPENDENCY"
+          FILE_DEPENDENCY=$(ls -c | head -n1)
+          dpkg -i "$FILE_DEPENDENCY"
+          done
+    fi
+
+    dpkg -i "$FILE"
 #else
 #    echo "Package $VAR1 it issss"
 fi
